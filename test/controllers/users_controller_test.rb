59c93199d1b5d84f9d36fbe5e044378ceccfb22c
root <admin@example.com>
2015-04-20 19:40:03 +0900

    mid commit

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test 'not actually a test' do
-    assert false
+  test '#signup_success' do
+    post :signup, {'username' => 'testuser', 'password' => 'testpass'}
+	json = JSON.parse(response.body)
+	assert json['user_name'] == 'testuser'
+	assert json['login_count'] == 1
+  end
+  
+  test '#signup_failure_user' do
+	post :signup, {'username' => 'test', 'password' => 'testpass'}
+	json = JSON.parse(response.body)
+	assert json['error_code'] == -1
+  end
+
+  test '#signup_failure_pwd' do
+	post :signup, {'username' => 'test', 'password' => 'testpass'}
+	json = JSON.parse(response.body)
+	assert json['error_code'] == -2
   end
   # test "the truth" do
   #   assert true
